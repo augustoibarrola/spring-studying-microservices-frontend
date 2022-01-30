e504e3eb1c5b9f134f14fd0bb17abc886343906e
augustoibarrola <augusto.t.ibarrola@gmail.com>
2022-01-30 16:54:53 -0500

    fixed margin

--- src/app/Shared/Pipes/phone-number.pipe.ts
+++ src/app/Shared/Pipes/phone-number.pipe.ts
@@ -6,8 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PhoneNumberPipe implements PipeTransform {
   transform(value: number): string {
     const phoneNum = "(" + value.toString().slice(0, 3) + ") - " + value.toString().slice(3, 6) + " - " + value.toString().slice(6);
-    
-    
     return phoneNum;
   }
 }
